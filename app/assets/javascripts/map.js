0e75fb4c061cb1620896a384c328fc17716cbbdb
finessestyle <finessestyle1@gmail.com>
2022-07-31 22:29:49 +0900

    20220731

--- app/assets/javascripts/map.js
+++
@@ -1,6 +0,0 @@
-function initMap(){
-    let map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: <%= @post.latitude %>, lng: <%= @post.longitude %> },
-    zoom: 15
-    });
-}
