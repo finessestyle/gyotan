2e204072aa272e062962ec4fdef4c60d30702423
finessestyle <finessestyle1@gmail.com>
2023-02-09 23:18:14 +0900

    20230209

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
 
   def top
     @posts = Post.all.order(created_at: :desc) 
-    @blogs = Blog.all.order(created_at: :desc)
+    # @blogs = Blog.all.order(created_at: :desc)
   end
   
   def show
