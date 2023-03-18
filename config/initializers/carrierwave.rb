de26d1f1c29862bd2d5aefe6b4121b98e051c578
finessestyle <finessestyle1@gmail.com>
2023-03-18 22:45:00 +0900

    20230318

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'gyotanbucket'
+    config.asset_host = 'https://s3.amazonaws.com/gyotanbucket'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
