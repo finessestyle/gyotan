c870e9a2fba0447c6e3815fa32dc6628d19fa79c
finessestyle <finessestyle1@gmail.com>
2023-03-20 11:05:47 +0900

    20230320

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    config.storage :fog
-    config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'gyotanbucket'
-    config.asset_host = 'https://s3.amazonaws.com/gyotanbucket'
-    config.fog_public = false
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: 'ap-northeast-3',
-      path_style: true
-    }
+  config.storage : fog
+  config.fog_provider = 'fog/aws'
+  config.fog_directory  = 'gyotanbucket'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+    region: 'ap-northeast-3',
+    path_style: true
+  }
 end 
