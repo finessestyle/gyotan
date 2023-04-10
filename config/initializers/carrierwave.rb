f8266a8348c6e6800c6fcbb69f2dd14500f5f4bd
finessestyle <finessestyle1@gmail.com>
2023-04-11 08:10:34 +0900

    20230411

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,8 +11,8 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+      aws_access_key_id: <%= ENV['AWS_ACCESS_KEY_ID'] %> , # 環境変数
+      aws_secret_access_key: <%= ENV['AWS_SECRET_ACCESS_KEY'] %>, # 環境変数
       region: 'ap-northeast-3',
       path_style: true
     }
