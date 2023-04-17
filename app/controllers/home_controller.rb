b307a8d4f4120e85c82479f8b81da67bfef51ee4
finessestyle <finessestyle1@gmail.com>
2023-04-17 22:47:08 +0900

    20230417

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,4 @@ class HomeController < ApplicationController
 
   def top
     @posts = Post.all.order(created_at: :desc) 
-    @spot = Spot.find(params[:id])
-    gon.api = Rails.application.credentials.open_weather_api_key
-  end
 end
