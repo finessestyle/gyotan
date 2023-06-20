f56caca714f928b01f7b20dc0c7cced877c363d4
finessestyle <finessestyle1@gmail.com>
2023-06-20 22:26:52 +0900

    20230620

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,21 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  # Don't care if the mailer can't send.
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV['ACTION_MAILER_USER'],
+    password:             ENV['ACTION_MAILER_KEY'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
