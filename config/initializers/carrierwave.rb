55cdde5e6186e047705041df1e3d6f228b4a3e3d
finessestyle <finessestyle1@gmail.com>
2023-07-24 21:02:28 +0900

    20230724

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,7 +13,7 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
-      region: 'ap-northeast-1',
+      region: 'ap-northeast-3',
       path_style: true
     }
   else
