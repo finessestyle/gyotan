945b274ede9307d09e7bbd15aee3e8c9ed8b9aaa
finessestyle <finessestyle1@gmail.com>
2023-09-22 17:06:41 +0900

    20230922

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: ENV['ACTION_MAILER_USER'], subject: "お問い合わせについて【自動送信】"
+    mail(
+      from: 'system@example.com',
+      to: ENV['ACTION_MAILER_USER'],
+      subject: "お問い合わせについて【自動送信】"
+    )
   end
 end
