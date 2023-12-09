6b1ba33653e7fee52d57b2ad7897e0912831c59e
finessestyle <finessestyle1@gmail.com>
2023-12-09 11:15:05 +0900

    20231209

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find_by(id: params[:id])
-    # impressionist(@post, nil, unique: [:ip_address])
+    impressionist(@post, nil, unique: [:ip_address])
     @user = @post.user
     @likes_count = Like.where(post_id: @post.id).count
   end
