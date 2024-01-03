b55a4bcab7e5c4599851cd71afedb11c5b204125
finessestyle <finessestyle1@gmail.com>
2024-01-04 00:25:51 +0900

    20240104

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
       charset: 'utf-8',
       separator: '|',
       description: '琵琶湖バス釣り情報サイト!!・琵琶湖バス釣り釣果情報・琵琶湖バス釣り場情報・バス釣り初心者向けブログ',
-      keywords: '琵琶湖,バス釣り,滋賀県,初心者,biwako,bassfishing,ブラックバス,釣り,gyotan,フィネス',
+      keywords: '琵琶湖,バス釣り,滋賀県,初心者,biwako,bassfishing,ブラックバス,釣り,gyotan,フィネス,バスフィッシング,',
       canonical: request.original_url,
       noindex: ! Rails.env.production?,
       icon: [                    #favicon、apple用アイコンを指定する
