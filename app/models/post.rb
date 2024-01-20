97a0b750a40b695dc30b803f842c92edcca37d8d
finessestyle <finessestyle1@gmail.com>
2024-01-20 21:56:54 +0900

    20240120

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   validates :user_id,{presence: true}
   validates :images, length: {maximum: 3}
   mount_uploaders :images, ImageUploader
-  # serialize :images, JSON
+  serialize :images, JSON
   
   def user
     return User.find_by(id: self.user_id)
