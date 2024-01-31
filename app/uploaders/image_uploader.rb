7148ec05fc2e3fbed344c46a9899e99e3cfa9eb9
finessestyle <finessestyle1@gmail.com>
2024-01-31 20:20:59 +0900

    20240131

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,5 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   attr_accessor :latitude, :longitude, :datetime
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   process :convert => 'jpg'
   
@@ -77,12 +79,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   def mimetype
     IO.popen(["file", "--brief", "--mime-type", path], in: :close, err: :close) { |io| io.read.chomp.sub(/image\//, "") }
   end
-  
-  
+
   def custom_optimize
     case mimetype
-    when "png" then pngquant
-    when "jpeg", "gif" then optimize(quality: 90)
+      when "png" then pngquant
+      when "jpeg", "gif" then optimize(quality: 90)
+    end
   end
 
   # Override the filename of the uploaded files:
