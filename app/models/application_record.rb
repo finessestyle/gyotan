8442856e44030ce80d3bb7cdaf842c754ad58374
finessestyle <finessestyle1@gmail.com>
2024-02-26 21:25:16 +0900

    20240226

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,15 +2,15 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def previous
-    user.posts.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
-    user.blogs.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
-    user.maps.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
+    user.posts.where('created_at < ? OR (created_at = ? AND id < ?)', created_at, created_at, id).order(created_at: :desc, id: :desc).first
+    user.blogs.where('created_at < ? OR (created_at = ? AND id < ?)', created_at, created_at, id).order(created_at: :desc, id: :desc).first
+    user.maps.where('created_at < ? OR (created_at = ? AND id < ?)', created_at, created_at, id).order(created_at: :desc, id: :desc).first
   end
-
+  
   def next
-    user.posts.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
-    user.blogs.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
-    user.maps.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first 
+    user.posts.where('created_at > ? OR (created_at = ? AND id > ?)', created_at, created_at, id).order(created_at: :asc, id: :asc).first
+    user.blogs.where('created_at > ? OR (created_at = ? AND id > ?)', created_at, created_at, id).order(created_at: :asc, id: :asc).first
+    user.maps.where('created_at > ? OR (created_at = ? AND id > ?)', created_at, created_at, id).order(created_at: :asc, id: :asc).first 
   end
   
 end
