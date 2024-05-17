739ec2f3bf3c187756ded527b8267a9709f52344
finessestyle <finessestyle1@gmail.com>
2024-05-17 23:12:56 +0900

    20240517

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,16 +5,11 @@ class HomeController < ApplicationController
 
   def top
     @monthly_top_catches = {}
-    # Catchの最初のデータから現在までの期間を取得
-    start_date = Post.minimum(:created_at).beginning_of_month
-    end_date = Time.now.end_of_month
-    current_date = start_date
-    while current_date <= end_date
-      month_start = current_date.beginning_of_month
-      month_end = current_date.end_of_month
-      top_catches = Post.where(created_at: month_start..month_end).order(length: :desc).limit(9)
-      @monthly_top_catches[month_start.strftime("%Y年%m月ランキング")] = top_catches
-      current_date = current_date.next_month
-    end
+    # 今月の期間を取得
+    month_start = Time.now.beginning_of_month
+    month_end = Time.now.end_of_month
+    # 今月のトップキャッチを取得
+    top_catches = Post.where(created_at: month_start..month_end).order(length: :desc).limit(9)
+    @monthly_top_catches[month_start.strftime("%Y年%m月ランキング")] = top_catches
   end
 end
